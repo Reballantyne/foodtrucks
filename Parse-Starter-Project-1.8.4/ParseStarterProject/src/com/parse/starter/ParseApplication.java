03e943596e3ca11f88499c046714fbacc51ba5ed
Paarth <paarth@Paarths-MacBook-Air.local>
2015-03-23 18:08:23 -0400

    skeleton code

---
+++ Parse-Starter-Project-1.8.4/ParseStarterProject/src/com/parse/starter/ParseApplication.java
@@ -0,0 +1,32 @@
+package com.parse.starter;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseCrashReporting;
+import com.parse.ParseUser;
+
+public class ParseApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Initialize Crash Reporting.
+    ParseCrashReporting.enable(this);
+
+    // Enable Local Datastore.
+    Parse.enableLocalDatastore(this);
+
+    // Add your initialization code here
+    Parse.initialize(this, "O55K2qUiFUhSJ1SfVOW3KQDQt8p5eGuORNFoQJHp", "4aY7YBIFKkouGEpZKzPWAFdfR43nzdn3w6qtVhZD");
+
+
+    ParseUser.enableAutomaticUser();
+    ParseACL defaultACL = new ParseACL();
+    // Optionally enable public read access.
+    // defaultACL.setPublicReadAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+  }
+}
