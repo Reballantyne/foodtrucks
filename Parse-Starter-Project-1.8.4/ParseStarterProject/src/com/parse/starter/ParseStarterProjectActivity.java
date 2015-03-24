4f22d8e46eb3d3d5c571586d0b8b236e3cbd45b7
Paarth <paarth@hnt.1919.wlanwireless-pennnet.upenn.edu>
2015-03-24 19:01:06 -0400

    sample code for parse

--- Parse-Starter-Project-1.8.4/ParseStarterProject/src/com/parse/starter/ParseStarterProjectActivity.java
+++ Parse-Starter-Project-1.8.4/ParseStarterProject/src/com/parse/starter/ParseStarterProjectActivity.java
@@ -3,6 +3,9 @@ package com.parse.starter;
 import android.app.Activity;
 import android.os.Bundle;
 import com.parse.ParseObject;
+import com.parse.*;
+import java.util.*;
+import android.util.*;
 
 import com.parse.ParseAnalytics;
 
@@ -12,11 +15,31 @@ public class ParseStarterProjectActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-//        Example on how to create an object in parse
-//        ParseObject testObject = new ParseObject("TestObject");
-//        testObject.put("foo", "cool beans");
-//        testObject.saveInBackground();
 
-		ParseAnalytics.trackAppOpenedInBackground(getIntent());
+//        This is some example code about how to add a user. Message Paarth or Nikila if confused.
+//        ParseObject me = new ParseObject("Customer");
+//        me.put("first_name", "rebecca");
+//        me.put("last_name", "ballantyne");
+//        me.put("password", "shouldbehashed");
+//        me.put("phoneNum", "555-555-5555");
+//        me.put("login", "rebecca");
+//        me.saveInBackground();
+
+//        This is some example code about how to query for a user. Message Paarth or Nikila if confused.
+//        ParseQuery<ParseObject> query = ParseQuery.getQuery("User");
+//        query.whereEqualTo("first_name", "rebecca");
+//        query.findInBackground(new FindCallback<ParseObject>() {
+//            public void done(List<ParseObject> user, ParseException e) {
+//                if (e == null) {
+//                    for (ParseObject u: user) {
+//                        System.out.println(u.get("last_name"));
+//                    }
+//                } else {
+//                    Log.d("score", "Error: " + e.getMessage());
+//                }
+//            }
+//        });
+
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
 	}
 }
