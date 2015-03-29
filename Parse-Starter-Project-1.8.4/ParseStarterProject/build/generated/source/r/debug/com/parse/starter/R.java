e527ffa3e3db295784afaf1e54a4aca0faa196fa
Ragini Raghavan <raginiraghavan@hnt349.apng.wharton.upenn.edu>
2015-03-29 15:50:53 -0400

    added login screen

--- Parse-Starter-Project-1.8.4/ParseStarterProject/build/generated/source/r/debug/com/parse/starter/R.java
+++ Parse-Starter-Project-1.8.4/ParseStarterProject/build/generated/source/r/debug/com/parse/starter/R.java
@@ -22,24 +22,40 @@ public final class R {
         public static final int specials=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f07000a;
-        public static final int filterButton=0x7f070003;
-        public static final int grid=0x7f070000;
-        public static final int gridBag=0x7f070001;
-        public static final int imageButton=0x7f070006;
-        public static final int imageButton2=0x7f070007;
-        public static final int imageButton3=0x7f070008;
-        public static final int imageButton4=0x7f070009;
-        public static final int listView=0x7f070005;
-        public static final int search=0x7f070002;
-        public static final int searchView=0x7f070004;
+        public static final int Without_Password=0x7f070005;
+        public static final int action_settings=0x7f070016;
+        public static final int btnLogin=0x7f070002;
+        public static final int btnRegister=0x7f070014;
+        public static final int button=0x7f070003;
+        public static final int filterButton=0x7f070009;
+        public static final int grid=0x7f070006;
+        public static final int gridBag=0x7f070007;
+        public static final int header=0x7f070000;
+        public static final int headerText=0x7f070001;
+        public static final int imageButton=0x7f07000c;
+        public static final int imageButton2=0x7f07000d;
+        public static final int imageButton3=0x7f07000e;
+        public static final int imageButton4=0x7f07000f;
+        public static final int link_to_login=0x7f070015;
+        public static final int link_to_register=0x7f070004;
+        public static final int listView=0x7f07000b;
+        public static final int reg_email=0x7f070011;
+        public static final int reg_fullname=0x7f070010;
+        public static final int reg_id=0x7f070012;
+        public static final int reg_password=0x7f070013;
+        public static final int search=0x7f070008;
+        public static final int searchView=0x7f07000a;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_register=0x7f030002;
+        public static final int main=0x7f030003;
     }
     public static final class menu {
-        public static final int menu_main=0x7f060000;
+        public static final int menu_login=0x7f060000;
+        public static final int menu_main=0x7f060001;
+        public static final int menu_register=0x7f060002;
     }
     public static final class string {
         public static final int action_settings=0x7f050000;
@@ -48,6 +64,8 @@ public final class R {
         public static final int hello_world=0x7f050003;
         public static final int parse_app_id=0x7f050004;
         public static final int parse_client_key=0x7f050005;
-        public static final int title_activity_main=0x7f050006;
+        public static final int title_activity_login=0x7f050006;
+        public static final int title_activity_main=0x7f050007;
+        public static final int title_activity_register=0x7f050008;
     }
 }
