a9d0f9b539982951d5ddcc02e80332572dc2dcd3
shkannan <shilpa.t.kannan@gmail.com>
2015-03-31 05:04:46 -0400

    Create FilterActivity.java
    Adding filter functionality

---
+++ Parse-Starter-Project-1.8.4/ParseStarterProject/src/com/parse/starter/FilterActivity.java
@@ -0,0 +1,51 @@
+package com.parse.starter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioGroup;
+
+public class FilterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_filter, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onFilterClicked(View view){
+        RadioGroup buttonID = (RadioGroup) findViewById(R.id.radio_group);
+        int checked = buttonID.getCheckedRadioButtonId();
+        if(checked > -1) {
+            Intent i = new Intent(this, MainActivity.class);
+            i.putExtra("filter", checked);
+            startActivity(i);
+        }
+    }
+}
