0aa3d891976543cb0afdc1c083c6955ac964ca6d
Reballantyne <rebal@sas.upenn.edu>
2015-04-11 22:02:28 -0400

    Changes relating to color scheme of application.

--- Parse-Starter-Project-1.8.4/ParseStarterProject/build/generated/source/r/debug/com/facebook/R.java
+++ Parse-Starter-Project-1.8.4/ParseStarterProject/build/generated/source/r/debug/com/facebook/R.java
@@ -90,13 +90,13 @@ public final class R {
 		public static final int box_count = 0x7f0b001a;
 		public static final int button = 0x7f0b001b;
 		public static final int center = 0x7f0b0020;
-		public static final int com_facebook_body_frame = 0x7f0b0046;
-		public static final int com_facebook_button_xout = 0x7f0b0048;
-		public static final int com_facebook_fragment_container = 0x7f0b0044;
-		public static final int com_facebook_login_activity_progress_bar = 0x7f0b0045;
-		public static final int com_facebook_tooltip_bubble_view_bottom_pointer = 0x7f0b004a;
-		public static final int com_facebook_tooltip_bubble_view_text_body = 0x7f0b0049;
-		public static final int com_facebook_tooltip_bubble_view_top_pointer = 0x7f0b0047;
+		public static final int com_facebook_body_frame = 0x7f0b0045;
+		public static final int com_facebook_button_xout = 0x7f0b0047;
+		public static final int com_facebook_fragment_container = 0x7f0b0043;
+		public static final int com_facebook_login_activity_progress_bar = 0x7f0b0044;
+		public static final int com_facebook_tooltip_bubble_view_bottom_pointer = 0x7f0b0049;
+		public static final int com_facebook_tooltip_bubble_view_text_body = 0x7f0b0048;
+		public static final int com_facebook_tooltip_bubble_view_top_pointer = 0x7f0b0046;
 		public static final int inline = 0x7f0b001e;
 		public static final int large = 0x7f0b0023;
 		public static final int left = 0x7f0b0021;
@@ -114,12 +114,12 @@ public final class R {
 		public static final int com_facebook_activity_layout = 0x7f030006;
 		public static final int com_facebook_login_fragment = 0x7f030007;
 		public static final int com_facebook_tooltip_bubble = 0x7f030008;
-		public static final int messenger_button_send_blue_large = 0x7f03000b;
-		public static final int messenger_button_send_blue_round = 0x7f03000c;
-		public static final int messenger_button_send_blue_small = 0x7f03000d;
-		public static final int messenger_button_send_white_large = 0x7f03000e;
-		public static final int messenger_button_send_white_round = 0x7f03000f;
-		public static final int messenger_button_send_white_small = 0x7f030010;
+		public static final int messenger_button_send_blue_large = 0x7f03000a;
+		public static final int messenger_button_send_blue_round = 0x7f03000b;
+		public static final int messenger_button_send_blue_small = 0x7f03000c;
+		public static final int messenger_button_send_white_large = 0x7f03000d;
+		public static final int messenger_button_send_white_round = 0x7f03000e;
+		public static final int messenger_button_send_white_small = 0x7f03000f;
 	}
 	public static final class string {
 		public static final int com_facebook_image_download_unknown_error = 0x7f080003;
