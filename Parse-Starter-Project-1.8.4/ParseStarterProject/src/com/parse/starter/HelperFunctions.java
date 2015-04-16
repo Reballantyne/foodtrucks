03d82524b465b8db07e19c93edb8d1e1fa5a2049
Ragini Raghavan <raginiraghavan@Raginis-MacBook-Pro.local>
2015-04-16 19:52:24 -0400

    added comments

--- Parse-Starter-Project-1.8.4/ParseStarterProject/src/com/parse/starter/HelperFunctions.java
+++ Parse-Starter-Project-1.8.4/ParseStarterProject/src/com/parse/starter/HelperFunctions.java
@@ -1,7 +1,15 @@
 package com.parse.starter;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 /**
  * Helper Functions used throughout the program
@@ -13,5 +21,38 @@ public class HelperFunctions {
         Toast toast = Toast.makeText(context, message, duration);
         toast.show();
     }
+
+
+    /*
+    public static Boolean displayDialog (String userNameOld, String passwordOld, Context context, ParseQuery<ParseObject> query){
+
+        final String password = passwordOld;
+        final String userName = userNameOld;
+        final boolean correctMatch = false;
+        try {
+            query.whereEqualTo("user_name", userName);
+            query.findInBackground(new FindCallback<ParseObject>() {
+                public void done(List<ParseObject> users, ParseException e) {
+                    //Checker the user information is correct
+                    if (e == null) {
+                        for (ParseObject u : users) {
+                            String passwordMatch = (String) u.get("password");
+                            if (passwordMatch.equals(password)) {
+                                LoginActivity.userNameSession = userName;
+                                correctMatch = true;
+                            } else {
+                                HelperFunctions.displayToast(context,"Incorrect Password/UserName");
+
+                            }
+
+                        }
+                    } else {
+                        Log.d("score", "Error: " + e.getMessage());
+                    }
+                }
+            });
+    }
+
+    */
 
 }
