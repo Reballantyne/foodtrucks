81bc5dee3a62e76fd3381586f48707c2a0b8a5d9
shkannan <shilpa.t.kannan@gmail.com>
2015-04-17 12:46:23 -0400

    Update ReviewItem.java
    Adding comments

--- Parse-Starter-Project-1.8.4/ParseStarterProject/src/com/parse/starter/ReviewItem.java
+++ Parse-Starter-Project-1.8.4/ParseStarterProject/src/com/parse/starter/ReviewItem.java
@@ -1,14 +1,14 @@
 package com.parse.starter;
 
 /**
- * Created by raginiraghavan on 4/12/15.
+ * This class enables storing of a review
+ * @author Shilpa Kannan, Srinidhi Raghavan
  */
 public class ReviewItem {
     public String user_name;
     public String review;
     public int likes;
     public String reviewID;
-
 
     public ReviewItem(String user_name, String review, int likes, String reviewId) {
         this.user_name = user_name;
