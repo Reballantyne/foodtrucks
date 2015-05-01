556dba0a11f8e72f666ed79842ade7ce4ed85780
shkannan <shilpa.t.kannan@gmail.com>
2015-05-01 14:08:33 -0400

    Update FavoritesActivity.java
    Fixing id to name issue

--- Parse-Starter-Project-1.8.4/ParseStarterProject/src/com/parse/starter/FavoritesActivity.java
+++ Parse-Starter-Project-1.8.4/ParseStarterProject/src/com/parse/starter/FavoritesActivity.java
@@ -73,7 +73,7 @@ public class FavoritesActivity extends Activity {
             favoritesNumber.setText(numFavorites + " favorite food trucks");
             //Create a ReviewItem for each review
             for (ParseObject f : favorites) {
-                String name = (String) f.get("foodtruck_id");
+                String name = (String) f.get("foodtruck_name");
          /*       ParseQuery<ParseObject> queryFTName = new ParseQuery<ParseObject>("FoodTruck");
                 queryFTName.whereEqualTo("object_id", id);
                 String foodtruckName = (String) queryFTName.find().get(0).get("name");*/
